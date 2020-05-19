6e22051940b354cb0221746eee60ca1f7664ec39
kota65535 <kota65535@gmail.com>
2020-05-19 11:02:05 +0100

    python3

--- server.py
+++ server.py
@@ -74,7 +74,7 @@ class WebSocket(tornado.websocket.WebSocketHandler):
 
     def loop(self):
         """Sends camera images in an infinite loop."""
-        sio = io.StringIO()
+        sio = io.BytesIO()
 
         if args.use_usb:
             _, frame = camera.read()
