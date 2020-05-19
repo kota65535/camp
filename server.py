77acf38a020e197688f38569fbdca17146bdaadc
kota65535 <kota65535@gmail.com>
2020-05-19 11:21:56 +0100

    iaaa

--- server.py
+++ server.py
@@ -10,45 +10,23 @@ import time
 import threading
 import webbrowser
 
-try:
-    import cStringIO as io
-except ImportError:
-    import io
+import io
 
 import tornado.web
 import tornado.websocket
 from tornado.ioloop import PeriodicCallback
 
+import picamera
+
+camera = picamera.PiCamera()
+camera.start_preview()
+
 # Hashed password for comparison and a cookie for login cache
 ROOT = os.path.normpath(os.path.dirname(__file__))
-with open(os.path.join(ROOT, "password.txt")) as in_file:
-    PASSWORD = in_file.read().strip()
-COOKIE_NAME = "camp"
-
 
 class IndexHandler(tornado.web.RequestHandler):
-
     def get(self):
-        if args.require_login and not self.get_secure_cookie(COOKIE_NAME):
-            self.redirect("/login")
-        else:
-            self.render("index.html", port=args.port)
-
-
-class LoginHandler(tornado.web.RequestHandler):
-
-    def get(self):
-        self.render("login.html")
-
-    def post(self):
-        password = self.get_argument("password", "")
-        if hashlib.sha512(password).hexdigest() == PASSWORD:
-            self.set_secure_cookie(COOKIE_NAME, str(time.time()))
-            self.redirect("/")
-        else:
-            time.sleep(1)
-            self.redirect(u"/login?error")
-
+        self.render("index.html", port=args.port)
 
 class ErrorHandler(tornado.web.RequestHandler):
     def get(self):
@@ -62,11 +40,8 @@ class WebSocket(tornado.websocket.WebSocketHandler):
 
         # Start an infinite loop when this is called
         if message == "read_camera":
-            if not args.require_login or self.get_secure_cookie(COOKIE_NAME):
-                self.camera_loop = PeriodicCallback(self.loop, 10)
-                self.camera_loop.start()
-            else:
-                print("Unauthenticated websocket request")
+            self.camera_loop = PeriodicCallback(self.loop, 10)
+            self.camera_loop.start()
 
         # Extensibility for other methods
         else:
@@ -76,12 +51,7 @@ class WebSocket(tornado.websocket.WebSocketHandler):
         """Sends camera images in an infinite loop."""
         sio = io.BytesIO()
 
-        if args.use_usb:
-            _, frame = camera.read()
-            img = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-            img.save(sio, "JPEG")
-        else:
-            camera.capture(sio, "jpeg", use_video_port=True)
+        camera.capture(sio, "jpeg", use_video_port=True)
 
         try:
             self.write_message(base64.b64encode(sio.getvalue()))
@@ -95,39 +65,19 @@ parser.add_argument("--port", type=int, default=8000, help="The "
                     "port on which to serve the website.")
 parser.add_argument("--resolution", type=str, default="low", help="The "
                     "video resolution. Can be high, medium, or low.")
-parser.add_argument("--require-login", action="store_true", help="Require "
-                    "a password to log in to webserver.")
-parser.add_argument("--use-usb", action="store_true", help="Use a USB "
-                    "webcam instead of the standard Pi camera.")
-parser.add_argument("--usb-id", type=int, default=0, help="The "
-                     "usb camera number to display")
 args = parser.parse_args()
 
-if args.use_usb:
-    import cv2
-    from PIL import Image
-    camera = cv2.VideoCapture(args.usb_id)
-else:
-    import picamera
-    camera = picamera.PiCamera()
-    camera.start_preview()
 
 resolutions = {"high": (1280, 720), "medium": (640, 480), "low": (320, 240)}
 if args.resolution in resolutions:
-    if args.use_usb:
-        w, h = resolutions[args.resolution]
-        camera.set(3, w)
-        camera.set(4, h)
-    else:
-        camera.resolution = resolutions[args.resolution]
+    camera.resolution = resolutions[args.resolution]
 else:
     raise Exception("%s not in resolution options." % args.resolution)
 
-handlers = [(r"/", IndexHandler), (r"/login", LoginHandler),
+handlers = [(r"/", IndexHandler),
             (r"/websocket", WebSocket),
-            (r"/static/password.txt", ErrorHandler),
             (r'/static/(.*)', tornado.web.StaticFileHandler, {'path': ROOT})]
-application = tornado.web.Application(handlers, cookie_secret=PASSWORD)
+application = tornado.web.Application(handlers)
 application.listen(args.port)
 
 webbrowser.open("http://localhost:%d/" % args.port, new=2)
